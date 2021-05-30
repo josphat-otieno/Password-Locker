c0e61244c877765594c72dc49c05b438d8267b8d
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-05-30 22:25:02 +0300

    created list to save users, method to define properties of a user and a method to save user objects

--- password.py
+++ password.py
@@ -1,2 +1,24 @@
 class User:
+    '''
+    class that generates new instances of users
+    '''
+    user_list=[]
+    '''
+    empty user list
+    '''
+
+    def __init__(self, username,password):
+        '''
+        A method to define properties of a user
+        '''
+        self.username=username
+        self.password=password
+
+    def save_user(self):
+        '''
+        save user method to save user objects
+        '''
+        User.user_list.append(self)
+
     
+        
